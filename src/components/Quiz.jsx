59233eaa2c5654b5a359362f514fffd0dfbc5b71
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-08-16 02:02:37 +0900

    クイズの実装

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const Quiz = () => {
+  const questions = [
+    {
+      question: "Javaの開発者は誰？",
+      options: [
+        "ジェームズ・ゴスリング",
+        "ビル・ゲイツ",
+        "スティーブ・ジョブズ",
+        "ラリー・ペイジ",
+      ],
+      answer: "ジェームズ・ゴスリング",
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerOptionClick = (option) => {
+    if (option === questions[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div>
+      {showScore ? (
+        <div>
+          <h2>
+            Score: {score} / {questions.length}
+          </h2>
+        </div>
+      ) : (
+        <div>
+          <h2>{questions[currentQuestion].question}</h2>
+          {questions[currentQuestion].options.map((option) => {
+            <button
+              key={option}
+              onClick={() => handleAnswerOptionClick(option)}
+            ></button>;
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
